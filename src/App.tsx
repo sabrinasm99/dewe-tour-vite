93247d4735be8ef064af18c4cf6214fb4123e316
Sabrina <sabrinasrimulyani@gmail.com>
2024-05-15 14:13:02 +0700

    feat: routing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" Component={HomePage} />;
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

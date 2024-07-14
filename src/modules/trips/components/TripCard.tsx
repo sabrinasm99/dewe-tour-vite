d3c849b943457a4250b29047240cf1a4ac2b4f24
Sabrina <sabrinasrimulyani@gmail.com>
2024-07-15 03:21:34 +0700

    feat: adjust cursor action on trip card

--- src/modules/trips/components/TripCard.tsx
+++ src/modules/trips/components/TripCard.tsx
@@ -1,38 +1,58 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import styles from "./styles/trip-card.module.css";
-
-type TripProps = {
-  id: number;
-  img: string;
-  title: string;
-  price: number;
-  country: string;
-  code: string;
-};
+import { MdEdit } from "react-icons/md";
+import { IoMdTrash } from "react-icons/io";
+import { TripProps } from "../../shared/types";
 
 export default function TripCard({ trip }: { trip: TripProps }) {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
-  const clickDetail = (id: number) => {
+  const clickDetail = (id: string) => {
     navigate(`/trip/${id}`);
   };
   return (
     <section
       key={trip.id}
       onClick={() => {
-        clickDetail(trip.id);
+        if (pathname !== "/admin-trip-list") {
+          clickDetail(trip.id);
+        }
       }}
       className={styles["card-container"]}
     >
-      <img src={trip.img} alt={trip.img} className={styles["card-img"]} />
+      <img
+        src={trip.cover_image}
+        alt={trip.cover_image}
+        className={styles["card-img"]}
+      />
       <h4 className={styles["card-title"]}>{trip.title}</h4>
       <article className={styles["price-and-place-container"]}>
         <h4 className={styles["card-price"]}>
           IDR. {trip.price.toLocaleString()}
         </h4>
-        <h4 className={styles["card-place"]}>{trip.country}</h4>
+        <h4 className={styles["card-place"]}>{trip.country.name}</h4>
       </article>
-      <p className={styles["card-quota"]}>{trip.code}</p>
+      <article
+        className={`${
+          pathname === "/admin-trip-list" ? "flex" : "hidden"
+        } mt-1`}
+      >
+        <section
+          onClick={() => navigate(`/edit-trip/${trip.id}`)}
+          className="flex items-center text-gray-600 hover:text-blue-500"
+        >
+          <MdEdit />
+          <p className="ml-1 font-medium">Edit</p>
+        </section>
+        <section className="flex items-center ml-3 text-gray-600 hover:text-red-500">
+          <IoMdTrash />
+          <p className="ml-1 font-medium">Remove</p>
+        </section>
+      </article>
+      <p className={styles["card-quota"]}>
+        {trip.booked_slots}/{trip.quota}
+      </p>
     </section>
   );
 }
--- src/modules/trips/features/trip-list/index.tsx
+++ src/modules/trips/features/trip-list/index.tsx
@@ -1,15 +1,20 @@
-import { tripList } from "../../../../fakedata";
+import { TripProps } from "../../../shared/types";
+import { useGetAllTrips } from "../../api";
 import TripCard from "../../components/TripCard";
 import styles from "./styles/trip.module.css";
 
 export default function TripList() {
+  const { data: trips } = useGetAllTrips();
+
   return (
     <section className={styles["trip-list-container"]}>
       <h2 className={styles["group-tour-text"]}>Group Tour</h2>
       <article className={styles["trip-list"]}>
-        {tripList.map((trip) => (
-          <TripCard key={trip.id} trip={trip} />
-        ))}
+        {trips && trips.length ? (
+          trips.map((trip: TripProps) => <TripCard key={trip.id} trip={trip} />)
+        ) : (
+          <></>
+        )}
       </article>
     </section>
   );

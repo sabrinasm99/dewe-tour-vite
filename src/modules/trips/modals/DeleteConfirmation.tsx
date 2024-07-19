22959dbe956959af1e8b91c46e66e571b2d7f85d
Sabrina <sabrinasrimulyani@gmail.com>
2024-07-19 23:28:18 +0700

    refactor: responsive delete confirmation

--- src/modules/trips/modals/DeleteConfirmation.tsx
+++ src/modules/trips/modals/DeleteConfirmation.tsx
@@ -22,11 +22,8 @@ export default function DeleteConfirmation({
 
   return (
     <>
-      <div
-        className="fixed rounded-md py-3 z-30 bg-white top-50% left-50% -translate-x-50% -translate-y-50% shadow-modal"
-        style={{ width: "500px" }}
-      >
-        <div className="border-b border-gray-300 px-3 pb-5 flex">
+      <div className="fixed rounded-md py-3 z-30 bg-white top-50% left-50% -translate-x-50% -translate-y-50% shadow-modal w-4/5 sm:w-3/5 md:w-1/2 lg:w-2/5 xl:w-1/3">
+        <div className="border-b border-gray-300 px-3 pb-3 sm:pb-5 flex">
           <h1 className="text-xl font-medium text-gray-700">
             Delete Confirmation
           </h1>
